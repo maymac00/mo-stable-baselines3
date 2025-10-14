cba36ebffe0975358a46f7a79f1106dad739a15a
maymac00 <arnau.mayoral10@gmail.com>
2025-10-14 15:50:22 +0200

    initial commit

--- tests/test_lppo.py
+++ tests/test_lppo.py
@@ -4,6 +4,264 @@ from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.vec_env import MoVecEnv, MoDummyVecEnv, DummyVecEnv, VecMonitor
 from stable_baselines3.lppo import LPPO, seqLPPO
 
+import numpy as np
+from numpy.random import default_rng
+from typing import SupportsFloat, Any
+
+import gymnasium as gym
+from gymnasium.core import ActType, ObsType, RenderFrame
+
+import sys
+import time
+import os
+
+from gymnasium.utils.env_checker import check_env
+
+class UnbreakableBottles:
+
+    NUM_CELLS = 5
+    NUM_INTERMEDIATE_CELLS = NUM_CELLS -2
+
+    AGENT_START = 0
+    AGENT_GOAL = 4
+
+    MAX_BOTTLES = 2
+    BOTTLES_TO_DELIVER = 2
+    DROP_PROBABILITY = 0.1 # 0.1
+
+    # define the ordering of the objectives
+    NUM_OBJECTIVES = 3
+    GOAL_REWARD = 0
+    IMPACT_REWARD = 1
+    PERFORMANCE_REWARD = 2
+
+    def __init__(self):
+        # initialize the problem - starting position is always at the home location
+        self.agent_location = self.AGENT_START
+        self.bottles_carried = 0
+        self.bottles_delivered = 0
+        self.num_bottles = [0 for i in range(self.NUM_INTERMEDIATE_CELLS)]
+        self.bottles_on_floor = 0
+        self.objectives = ['GOAL_REWARD', 'IMPACT_REWARD', 'PERFORMANCE_REWARD']
+        self.initial_rewards = [0, 0, 0]
+        self.rewards = dict(zip(self.objectives, self.initial_rewards))
+        self.actions = ['left', 'right', 'pick_up_bottle']
+        self.actions_index = {'left': 0, 'right': 1, 'pick_up_bottle': 2}
+        self.terminal_state = False
+        self.rng = default_rng()
+
+    def get_state(self):
+        index = self.agent_location + (self.NUM_CELLS * self.bottles_carried)
+        # convert bottle states to an int
+        bottle_state = 0
+        multiplier = 1
+        for i in range(self.NUM_INTERMEDIATE_CELLS):
+            if self.num_bottles[i] > 0:
+                bottle_state += multiplier
+            multiplier *= 2
+        index += bottle_state * (self.NUM_CELLS * (self.MAX_BOTTLES + 1))
+        if self.bottles_delivered > 0:
+            index += 120
+        return index
+
+    def env_init(self):
+        # initialize the problem - starting position is always at the home location
+        self.agent_location = self.AGENT_START
+        self.bottles_carried = 0
+        self.bottles_delivered = 0
+        self.num_bottles = [0 for i in range(self.NUM_INTERMEDIATE_CELLS)]
+        self.bottles_on_floor = 0
+        self.terminal_state = False
+
+    def env_start(self):
+        # Set up the environment for the start of a new episode
+        self.agent_location = self.AGENT_START
+        self.bottles_carried = 0
+        self.bottles_delivered = 0
+        self.num_bottles = [0 for i in range(self.NUM_INTERMEDIATE_CELLS)]
+        self.bottles_on_floor = 0
+        self.terminal_state = False
+        observation = (self.agent_location, self.bottles_carried, self.num_bottles, self.bottles_delivered)
+        return observation
+
+    def env_clean_up(self):
+        # starting position is always the home location
+        self.agent_location = self.AGENT_START
+
+    def potential(self, bottle_count):
+        # Returns the value of the potential function for the current state, which is the
+        # difference between the red-listed attributes of that state and the initial state.
+        # In this case, its -1 if any intermediate cells contain bottles.
+        for i in range(self.NUM_INTERMEDIATE_CELLS):
+            if bottle_count[i] > 0:
+                return -1
+        return 0
+
+    def potential_difference(self, old_state, new_state):
+        # Calculate a reward based off the difference in potential between the current
+        # and previous state
+        return self.potential(new_state) - self.potential(old_state)
+
+    def env_step(self, action):
+        # update the agent's position within the environment based on the specified action
+        old_state = self.num_bottles.copy()
+        self.bottles_delivered_this_step = 0
+        # calculate the new state of the environment
+        # Moving left
+        if action == 'left':
+            if self.agent_location > 0:
+                self.agent_location -= 1
+                if self.agent_location > 0 and self.bottles_carried == self.MAX_BOTTLES \
+                        and self.rng.uniform(0,1) <= self.DROP_PROBABILITY:
+                    # oops, we dropped a bottle
+                    self.num_bottles[self.agent_location - 1] += 1
+                    self.bottles_carried -= 1
+        # Moving right
+        if action == 'right':
+            if self.agent_location < self.AGENT_GOAL:
+                self.agent_location += 1
+                if self.agent_location == self.AGENT_GOAL:
+                    # deliver bottles
+                    self.bottles_delivered_this_step = min(self.MAX_BOTTLES - self.bottles_delivered, self.bottles_carried)
+                    self.bottles_delivered += self.bottles_delivered_this_step
+                    self.bottles_carried -= self.bottles_delivered_this_step
+                elif self.bottles_carried == self.MAX_BOTTLES and self.rng.uniform(0,1) <= self.DROP_PROBABILITY:
+                    # oops, we dropped a bottle
+                    self.num_bottles[self.agent_location - 1] += 1
+                    self.bottles_carried -= 1
+        # Pick up bottle
+        if action == 'pick_up_bottle':
+            if self.agent_location == self.AGENT_START and self.bottles_carried < self.MAX_BOTTLES:
+                self.bottles_carried += 1
+            elif self.agent_location < self.AGENT_GOAL and self.bottles_carried < self.MAX_BOTTLES \
+                    and self.num_bottles[self.agent_location - 1] > 0:
+                self.num_bottles[self.agent_location - 1] -= 1
+                self.bottles_carried += 1
+        # is this a terminal state?
+        self.terminal_state = self.bottles_delivered >= self.BOTTLES_TO_DELIVER
+        # set up the reward vector
+        new_bottles_on_floor = 0
+        for i in range(self.NUM_INTERMEDIATE_CELLS):
+            new_bottles_on_floor += self.num_bottles[i]
+        self.rewards['IMPACT_REWARD'] = self.potential_difference(old_state, self.num_bottles)
+        self.bottles_on_floor = new_bottles_on_floor
+        step_reward = -1 + self.bottles_delivered_this_step * 25
+        self.rewards['GOAL_REWARD'] = step_reward
+        if (not(self.terminal_state)):
+            self.rewards['PERFORMANCE_REWARD'] = step_reward
+        else:
+            self.rewards['PERFORMANCE_REWARD'] = step_reward - 50 * self.bottles_on_floor
+        # wrap new observation
+        observation = (self.agent_location, self.bottles_carried, self.num_bottles, self.bottles_delivered)
+        return self.rewards, observation
+
+    def is_terminal(self):
+        return self.terminal_state
+
+    def visualise_environment(self):
+        # print out an ASCII representation of the environment, for use in debugging
+        print()
+        print('----------------------------------')
+        # display agent
+        print('Agent at cell ' + str(self.agent_location) + ' carries ' + str(self.bottles_carried) + ' bottles.')
+        print()
+        # print cell labels / contents
+        print('Source S\t')
+        for i in range(self.NUM_INTERMEDIATE_CELLS):
+            print('Bottles in intermediate cell ' + str(i + 1) + ' = ' + str(self.num_bottles[i]))
+        print('Destination D, bottles delivered = ' + str(self.bottles_delivered))
+        print('----------------------------------')
+
+class GymUnbreakableBottles(UnbreakableBottles, gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    action2string = {
+        0: "left",
+        1: "right",
+        2: "pick_up_bottle"
+    }
+
+    sting2action = {
+        "left": 0,
+        "right": 1,
+        "pick_up_bottle": 2
+    }
+
+    def __init__(self, mode: str = "scalarised",WS=[1,1] , we: float = 3.0, normalised_obs: bool = True):
+
+        super(GymUnbreakableBottles, self).__init__()
+        super().__init__()
+        self.mode = mode
+        self.we = we
+        self.env_start()
+        self.step_count = 0
+        self.max_steps = 50
+        self.normalised_obs = normalised_obs
+        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(4,), dtype=np.float64) if self.normalised_obs else gym.spaces.MultiDiscrete([5, 3, 2, 2**3])
+        self.action_space = gym.spaces.Discrete(3)
+
+        # render
+        self.window = None
+        self.windows_size = 300
+        self.WS = WS
+
+    def step(
+            self, action: ActType
+    ) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
+        if not isinstance(action, str):
+            action = self.action2string[action]
+        rewards, observation = self.env_step(action)
+        self.step_count += 1
+        tr = self.step_count >= self.max_steps
+        tm = self.is_terminal()
+
+        #r0 = 0 if not (tm or tr) else -50*self.bottles_on_floor
+        #r1 = -1 + self.bottles_delivered_this_step*25
+
+        r0 = rewards['IMPACT_REWARD']  # 0 if not (tm or tr) else -50 * self.bottles_on_floor
+        r1 = rewards['GOAL_REWARD']  # -1 + self.bottles_delivered_this_step * 25
+
+        if self.mode == "scalarised":
+            rewards = float(r0*self.WS[1] + r1*self.WS[0])
+        else:
+            rewards = np.array([r0, r1]).astype(np.float32)
+
+        info = {'Individual': r1 ,'Etical': r0}
+
+
+        return np.array(self.prep_obs(observation)), rewards, tm or tr , False , info
+
+
+    def prep_obs(self, obs):
+        obs = np.array([obs[0], obs[1], obs[3], 2**obs[2][0] + 2**obs[2][1] + 2**obs[2][2]] )
+        if self.normalised_obs:
+            return obs / np.array([5 - 1, 3 - 1, 2 - 1, 2 ** 3 - 1])
+        return obs
+
+    def render(self) -> RenderFrame | list[RenderFrame] | None:
+        return NotImplementedError
+
+    def reset(
+            self,
+            *,
+            seed: int | None = None,
+            options: dict[str, Any] | None = None,
+    ) -> tuple[ObsType, dict[str, Any]]:
+
+        super().reset(seed=seed)
+
+        if seed is not None:
+            self.rng = default_rng(seed=seed)
+        self.step_count = 0
+        self.env_clean_up()
+        self.env_init()
+        return np.array(self.prep_obs(self.env_start())), {}
+
+    def setWeights(self,WS):
+        self.we  = WS[1]
+        self.WS = WS
+
+
 
 def linear_schedule(initial_value):
     def func(progress_remaining):
@@ -30,20 +288,9 @@ class EntropyScheduleCallback(BaseCallback):
         return True
 
 
-from EthicalGatheringGame.MultiAgentEthicalGathering import MAEGG
-from EthicalGatheringGame.presets import tiny
-
-tiny['n_agents'] = 1
-tiny["reward_mode"] = "vectorial"
-tiny["objective_order"] = "ethical_first"
-tiny["inequality_mode"] = "loss"
-tiny["efficiency"] = [0.6]
-tiny["donation_capacity"] = 15
-tiny["survival_threshold"] = 30
-
 def make_env():
     def _init():
-        return MAEGG(**tiny)
+        return GymUnbreakableBottles(mode="vector")
     return _init
 env = MoDummyVecEnv([make_env() for _ in range(5)],)
 env = MoVecMonitor(env)
@@ -65,13 +312,13 @@ args = {
     "eta_values": 1e-3*2,
     "policy_kwargs": {
         "n_objectives": 2,
-        "net_arch": dict(pi=[256, 128], vf=[256, 128])
+        "net_arch": dict(pi=[64, 64], vf=[64, 64])
     },
     "verbose": 1,
     "device": "cuda:0",
-    "tensorboard_log": "runs",
+    #"tensorboard_log": "runs",
     "clip_range_vf": 0.2,
-    "gamma": 0.8,
+    "gamma": 1.0,
     "normalize_advantage": True,
     "tolerance": 0.01,
     "recent_loses_len": 80,
@@ -84,15 +331,11 @@ model = seqLPPO("MoMlpPolicy", env, 2, **args)
 #model = PPO("MlpPolicy", env, **args)
 
 model.learn(total_timesteps=100000, log_interval=1)
-model.save("model")
+"""model.save("test_model")
 
 #model.save("test")
-env = MAEGG(**tiny)
-env.toggleTrack(True)
-env.toggleStash(True)
-
-
-"""model = LPPO.load("test", env=env)
+env = GymUnbreakableBottles(mode="scalarised", WS=[1,3])
+model = LPPO.load("test", env=env)
 
 for ep in range(150):
     obs, _ = env.reset()
